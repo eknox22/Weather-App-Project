2b986c84d17a967de42f5ae8f4e03c5008839955
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-05-03 20:50:26 +0100

    Added the search functionally using the API

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-function refreshWeather(response) {}
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
 
 function searchCity(city) {
   let apiKey = "t61a3de96178c340b47o7ff32962bb7f";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&{unis=metric}"`;
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&{unis=metric}`;
   axios.get(apiURL).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Bristol");
