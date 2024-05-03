f3f2bd56c098c0dcfb7a39a02dc0392c21060bfa
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-05-03 19:22:39 +0100

    Added api key and URL

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+function refreshWeather(response) {}
+
+function searchCity(city) {
+  let apiKey = "t61a3de96178c340b47o7ff32962bb7f";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&{unis=metric}"`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
